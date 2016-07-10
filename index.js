04700d0e7a6cd8c3f74f7f4055aa05a9cd9b1a55
vishalmishra4u <mishravishal0912@gmail.com>
2016-07-10 18:05:03 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3000);
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
